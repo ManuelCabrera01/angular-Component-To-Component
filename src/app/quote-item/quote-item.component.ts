2ccc612237f350c9dc23cb7dbd7b77b5de26eff5
Manuel <m.a.c.p90@gmail.com>
2017-11-06 20:01:58 -0500

    quote component created

---
+++ src/app/quote-item/quote-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-item',
+  templateUrl: './quote-item.component.html',
+  styleUrls: ['./quote-item.component.css'],
+  // encapsulation: ViewEncapsulation.None
+})
+export class QuoteItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
