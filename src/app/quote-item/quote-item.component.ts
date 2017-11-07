451dc2ea1f609af29f710e7dbbff5e3bdbe80c0f
Manuel <m.a.c.p90@gmail.com>
2017-11-06 20:54:00 -0500

    delete button added

--- src/app/quote-item/quote-item.component.ts
+++ src/app/quote-item/quote-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output,  EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quote-item',
@@ -7,10 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
   // encapsulation: ViewEncapsulation.None
 })
 export class QuoteItemComponent implements OnInit {
- @Input() quote: any;
+ @Input() quote: any
+ @Output() onDelete = new EventEmitter<string>();
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+  onQuoteDelete () {
+   this.onDelete.emit(this.quote.id);
+ }
 
 }
