2daeb5c2647b8aed1425ae3ea14dfa7d763b1e5a
Manuel <m.a.c.p90@gmail.com>
2017-11-06 20:22:31 -0500

    quote component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuoteItemComponent } from './quote-item/quote-item.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteItemComponent
+    QuoteItemComponent,
+    QuoteListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/quote-list/quote-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-list',
+  templateUrl: './quote-list.component.html',
+  styleUrls: ['./quote-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class QuoteListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
